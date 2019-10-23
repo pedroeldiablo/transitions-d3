d86d3b56f1616f65eb5be433940e4cc42a1490c4
Peter Williams <pedroeldiablo@yahoo.com>
2019-10-23 22:14:20 +0100

    adding on transitions for start and end transitions

--- app.js
+++ app.js
@@ -42,6 +42,15 @@ d3.select('svg')
   })
   .attr('fill', 'purple');
 
+d3.select('svg')
+  .append('text')
+  .classed('title', true)
+  .text(`Birth Data in ${minYear}`)
+  .attr('x', width /2)
+  .attr('y', 30)
+  .style('text-anchor', 'middle')
+  .style('font-size', '2em');
+
 d3.select('input')
   .on('input', function() {
     var year = +d3.event.target.value;
@@ -53,6 +62,21 @@ d3.select('input')
       .duration(2000)
       .ease(d3.easeLinear)
       .delay(( d, i) => i * 150 )
+      .on('start', function(d, i) {
+        if (i === 0) {
+          d3.select('.title')
+            .text(`Updating to ${year} data...`);
+
+        }
+      })
+        
+      .on('end', function(d, i, nodes) {
+        if ( i === nodes.length -1 ) {
+          d3.select('.title')
+            .text(`Birth Data in ${year}`);
+        }
+      })
+       
       .attr('height', function(d) {
         return height - yScale(d.births);
       })
